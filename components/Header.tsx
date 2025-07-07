b22b9cd3a6b0c2f98efa0df9c98fa1470943473c
ytgap <nelsonbilly@yahoo.com>
2025-07-06 20:53:18 -0500

    Create Header.tsx

---
+++ components/Header.tsx
@@ -0,0 +1,12 @@
+export default function Header() {
+  return (
+    <header style={{ marginBottom: 32 }}>
+      <h1 style={{ fontWeight: 800, fontSize: 36, color: '#FF0000', letterSpacing: 1 }}>
+        YTGap
+      </h1>
+      <p style={{ fontWeight: 500, color: '#374151' }}>
+        Discover YouTube search terms with high interest & low competition!
+      </p>
+    </header>
+  );
+}
